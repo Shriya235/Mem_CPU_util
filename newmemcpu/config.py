95f1fb79672b686631935a6746faafbea421397d
Shriya235 <65900306+Shriya235@users.noreply.github.com>
2020-05-26 22:18:36 +0530

    Add files via upload

---
+++ newmemcpu/config.py
@@ -0,0 +1,4 @@
+logfile='alpha_board_router_stats.txt'
+
+avg=['Average:        all','Average:          0','Average:          1','Average:          2','Average:          3']
+memory= "Mem:      "
---
+++ newmemcpu/main_prog.py
@@ -0,0 +1,5 @@
+import config as conf
+import commonfunc as cf
+
+
+cf.Data_preparation(conf.logfile,conf.avg,conf.memory)
