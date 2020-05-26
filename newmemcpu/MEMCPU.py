e4b4f3a1bffe8378feabefd529cd4c87ad29c5e6
Shriya235 <65900306+Shriya235@users.noreply.github.com>
2020-05-26 22:28:21 +0530

    Update MEMCPU.py

--- newmemcpu/MEMCPU.py
+++ newmemcpu/MEMCPU.py
@@ -8,7 +8,7 @@ import datetime as dt
 import itertools
 from  datetime import datetime
 
-def mem(sub,logfile):
+def mem(sub,logfile):                       # subroutine for memory utilisation
     mem = []
     newlistmem = []
     memtime=[]
@@ -20,7 +20,7 @@ def mem(sub,logfile):
                 mem.extend(re.split(r'[|\s]\s*', line))                                      
     for (value,total) in zip(mem[2::10],mem[1::10]):
         new_elem = int(value)/int(total)                                
-        newlistmem.append(new_elem*100)
+        newlistmem.append(new_elem*100)             #newlistmem has the final values
     for d,t in zip(mem[7::10],mem[8::10]):
        tim.append(d+" "+t)
     date_obj = []
@@ -30,7 +30,7 @@ def mem(sub,logfile):
     Data_Printing(newlistmem,substr)
     Data_Plotting(dates,newlistmem,"Memory Utilisation")
     
-def cpu(str1,logfile):
+def cpu(str1,logfile):                          #subroutine for cpu utilisation
     cpu=[]
     newlistcpu=[]
     cputime=[]
@@ -41,7 +41,7 @@ def cpu(str1,logfile):
             if line.find(substr1)!= -1:
                 cpu.extend(re.split(r'[|\s]\s*', line))
     for value in cpu[7::11]:
-        newlistcpu.append(100-float(value))
+        newlistcpu.append(100-float(value))         #newlistcpu has the final values
     for d,t in zip(cpu[8::11],cpu[9::11]):
         cputime.append(d+" "+t)
     date_obj = []
@@ -51,7 +51,7 @@ def cpu(str1,logfile):
     Data_Printing(newlistcpu,str1)
     Data_Plotting(dates,newlistcpu,"CPU Utilisation for Avg "+valu[1])
   
-def Data_Printing(value,string1):
+def Data_Printing(value,string1):                  #prints the max,min and avg of the values
     if string1 == "Mem:      ":
        print("Maximum memory utilisation: ",max(value),"%")
        print("Minimum memory utilisation: ",min(value),"%")
@@ -62,7 +62,7 @@ def Data_Printing(value,string1):
        print("Minimum CPU utilisation for Average "+val[1]+": ",min(value),"%")
        print("Average CPU utilisation for Average "+val[1]+": ",statistics.mean(value),"%")
 
-def Data_Plotting(x,y,title):
+def Data_Plotting(x,y,title):                       #plots the mem and cpu utilisation over time
     ax=plt.gca()
     xfmt = md.DateFormatter('%Y-%m-%d %H:%M:%S')
     ax.xaxis.set_major_formatter(xfmt)
@@ -74,7 +74,7 @@ def Data_Plotting(x,y,title):
     plt.show()                                                             
     plt.savefig('utilisation.png')
     
-def Data_preparation(fname,avg,memory):
+def Data_preparation(fname,avg,memory):             #calls for the required memory or cpu utilisation methods
     mem(memory,fname)    
     for i in avg:
         cpu(i,fname)
